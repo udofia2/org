f39f1622865514b99375dfd9da82943e5f4874cc
Abasiodiong Udofia <enalsy22@gmail.com>
2024-07-09 05:14:13 +0100

    your commit message

--- src/controllers/organizations/update.ts
+++ src/controllers/organizations/update.ts
@@ -1,14 +1,12 @@
-import { StringExpressionOperator } from 'mongoose';
 import { dataSource } from '../../orm/dbCreateConnection';
 
 import { Organization } from '../../orm/entities/organization/Organization';
 
-export const updatePackageStatus = async (packageId: string,) => {
+export const updatePackageStatus = async (packageId: string) => {
   const OrganizationRepository = dataSource.getRepository(Organization);
 
   const packageToUpdate = await OrganizationRepository.findOne({ where: { orgId: packageId } });
   if (packageToUpdate) {
-    // packageToUpdate.status = newStatus;
     await OrganizationRepository.save(packageToUpdate);
   }
 };
