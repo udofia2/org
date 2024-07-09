c7a4b5cc28dafee7c4eedb8d7bc80eb0b6505e94
Abasiodiong Udofia <enalsy22@gmail.com>
2024-07-09 09:29:08 +0100

    fixed(base_url):update base url

--- src/routes/pages/root.ts
+++ src/routes/pages/root.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).header('Content-Type', 'text/html').send(`<h4>🚚📦📦 PACKAGE TRACKING RESTful API</h4>`);
+  res.status(200).header('Content-Type', 'text/html').send(`<h4>🗂️💼🎯🖼️  ORGANIZATION MANAGEMENT RESTful API</h4>`);
 });
 
 export default router;
